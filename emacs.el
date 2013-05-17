33f88f800d7ffcb3441e57303bb64952045f6bfd
Asher Langton <langton2@llnl.gov>
2013-05-17 10:18:27 -0700

    new keybinding for emacs

--- emacs.el
+++ emacs.el
@@ -331,6 +331,7 @@
 (global-set-key "\C-ct" 'toggle-tabs-mode)
 (global-set-key "\C-cw" 'toggle-current-window-dedication)
 (global-set-key "\C-c " 'toggle-show-trailing-whitespace)
+(global-set-key "\C-x9" 'other-frame)
 ;; Alternate ways to get M-x
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-xm" 'execute-extended-command)
