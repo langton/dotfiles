4a49d6871c13bf462c1df99907ed2422b70bf501
Asher Langton <langton@juniper.net>
2015-04-24 14:47:54 -0700

    changed dir tracking to use ip address

--- bashrc.sh
+++ bashrc.sh
@@ -46,15 +46,7 @@ case $TERM in
 function set-eterm-dir {
     echo -e "\033AnSiTu" "$LOGNAME" # $LOGNAME is more portable than using whoami.
     echo -e "\033AnSiTc" "$(pwd)"
-    if [ $(uname) = "SunOS" ]; then
-	# The -f option does something else on SunOS and is not needed anyway.
-        hostname_options="";
-    else
-        hostname_options="-f";
-    fi
-    echo -e "\033AnSiTh" "$(hostname $hostname_options)" # Using the -f option can
-                                                             # cause problems on some OSes.
-    history -a # Write history to disk.
+    echo -e "\033AnSiTh" "$(hostname -i)"
 }
 
 # Track directory, username, and cwd for remote logons.
