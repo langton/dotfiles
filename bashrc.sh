cf838367df0cc0fb78c8da60f2143ad41b5aa891
Asher Langton <langton@juniper.net>
2015-04-24 17:03:21 -0700

    fixes for remote host name (TRAMP compatibility)

--- bashrc.sh
+++ bashrc.sh
@@ -43,15 +43,30 @@ case $TERM in
 
 #export TERMINFO=${HOME}/.terminfo
 
+# do not call directly; should only be set as PROMPT_COMMAND via following
 function set-eterm-dir {
     echo -e "\033AnSiTu" "$LOGNAME" # $LOGNAME is more portable than using whoami.
     echo -e "\033AnSiTc" "$(pwd)"
-    echo -e "\033AnSiTh" "$(hostname -i)"
+    # check to see if hostname -i matches val in SSH_CONNECTION
+    echo -e "\033AnSiTh" "$tramp_host"
 }
 
 # Track directory, username, and cwd for remote logons.
-if [ -n "$SSH_CLIENT" -a "$TERM" = "eterm-color" ]; then
+if [ -n "$SSH_CONNECTION" -a "$TERM" = "eterm-color" ]; then
     export TERMCAP="eterm-color:li#67:co#118:cl=\E[H\E[J:cd=\E[J:bs:am:xn:cm=\E[%i%d;%dH:nd=\E[C:up=\E[A:ce=\E[K:ho=\E[H:pt:al=\E[L:dl=\E[M:DL=\E[%dM:AL=\E[%dL:cs=\E[%i%d;%dr:sf=^J:dc=\E[P:DC=\E[%dP:IC=\E[%d@:im=\E[4h:ei=\E[4l:mi::so=\E[7m:se=\E[m:us=\E[4m:ue=\E[m:md=\E[1m:mr=\E[7m:me=\E[m:UP=\E[%dA:DO=\E[%dB:LE=\E[%dD:RI=\E[%dC:kl=\EOD:kd=\EOB:kr=\EOC:ku=\EOA:kN=\E[6~:kP=\E[5~:@7=\E[4~:kh=\E[1~:mk=\E[8m:cb=\E[1K:op=\E[39;49m:Co#8:pa#64:AB=\E[4%dm:AF=\E[3%dm:cr=^M:bl=^G:do=^J:le=^H:ta=^I:se=\E[27m:ue=\E24m:kb=^?:kD=^[[3~:sc=\E7:rc=\E8:r1=\Ec:"
     export TERM=eterm-color # force termcap to be read
+    ssh_str_tmp=( $SSH_CONNECTION )
+    ssh_tmp=${ssh_tmp[2]}
+    ip_tmp=$(dig +short $(hostname -f))
+    # use hostname iff it resolves; grab IP address from ssh string otherwise
+    if [[ -z $ip_tmp && -n $ssh_tmp ]]
+    then
+        tramp_host=$ssh_tmp
+    else
+        tramp_host=$(hostname -f)
+    fi
+    unset ssh_str_tmp
+    unset ssh_tmp
+    unset ip_tmp
     PROMPT_COMMAND=set-eterm-dir
 fi
